df60c2ff8e4f4b0fd541aa84f30822b0ebac6485
svoh86 <sms-86@mail.ru>
2024-01-31 16:27:20 +0300

    4. Микросервисные системы [#338434]

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -1,17 +1,22 @@
 package ru.job4j.site.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.netty.util.internal.ConcurrentSet;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.InterviewsService;
-import ru.job4j.site.service.NotificationService;
+import ru.job4j.site.dto.InterviewDTO;
+import ru.job4j.site.dto.ProfileDTO;
+import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -23,6 +28,7 @@ public class IndexController {
     private final InterviewsService interviewsService;
     private final AuthService authService;
     private final NotificationService notifications;
+    private final ProfilesService profilesService;
 
     @GetMapping({"/", "index"})
     public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
@@ -41,7 +47,14 @@ public class IndexController {
         } catch (Exception e) {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
-        model.addAttribute("new_interviews", interviewsService.getByType(1));
+        List<InterviewDTO> interviewDTOList = interviewsService.getByType(1);
+        Set<ProfileDTO> profileDTOSet = ConcurrentHashMap.newKeySet();
+        for (InterviewDTO interviewDTO : interviewDTOList) {
+            Optional<ProfileDTO> profileById = profilesService.getProfileById(interviewDTO.getSubmitterId());
+            profileById.ifPresent(profileDTOSet::add);
+        }
+        model.addAttribute("new_interviews", interviewDTOList);
+        model.addAttribute("profiles", profileDTOSet);
         return "index";
     }
 }
